367ed1e7b387d4d7352d1444f001033786f79812
Robert Schütte <schuetterobert@gmail.com>
2019-04-13 01:02:29 +0200

    REmove debug printline

--- src/lib.rs
+++ src/lib.rs
@@ -340,8 +340,6 @@ impl Executer {
         // - Check if all connections used in blocks exist
         // - Check if all connections use in blocks are valid
 
-        println!("{:?}", self.code);
-
         self.code_ok = true;
         Ok(())
     }
